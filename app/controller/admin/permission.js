fadd24806c4e4063ac8ae3820cb85ce9fcb9a381
Stewart <7352287+ste-wart@user.noreply.gitee.com>
2021-01-22 20:50:26 +0800

    permissionCRUD

--- app/controller/admin/permission.js
+++ app/controller/admin/permission.js
@@ -45,60 +45,106 @@ class PermissionController extends Controller {
 		const enable = data.enable
 
 		let flagOne = await ctx.service.permission.edit(id, data)
-    let flagTwo = await ctx.service.permission.updateSecondEnable(id,enable)
+		let flagTwo = await ctx.service.permission.updateSecondEnable(
+			id,
+			enable
+		)
 
-		if (flagOne&&flagTwo) {
-      SUCCESS.msg= "修改成功"
+		if (flagOne && flagTwo) {
+			SUCCESS.msg = '修改成功'
 			ctx.body = SUCCESS
 		} else {
 			ctx.body = ERROR
 		}
 	}
-  /**
+	/**
 	 * @summary 添加权限
 	 * @description 添加一个权限信息
 	 * @router post /admin/permission/add
 	 * @request body addPermission
 	 * @response 200 RESULT
 	 */
-  async add(){
-    const { ctx } = this
+	async add() {
+		const { ctx } = this
 		let data = ctx.request.body
-    console.log(data); 
-    let result =await ctx.service.permission.add(data)
-    if (result) {
-      SUCCESS.msg = "添加成功"
+		console.log(data)
+		let result = await ctx.service.permission.add(data)
+		if (result) {
+			SUCCESS.msg = '添加成功'
 			ctx.body = SUCCESS
 		} else {
 			ctx.body = ERROR
 		}
-  }
-  /**
-	 * @summary 删除权限(未完成)
+	}
+	/**
+	 * @summary 删除权限
 	 * @description 删除一个权限信息
 	 * @router get /admin/permission/delete
 	 * @request query integer id 权限id
 	 * @response 200 RESULT
 	 */
-  async delete(){
-    const { ctx } = this
-    const {id} = ctx.query
-    console.log(id);
-    const pid = await ctx.service.permission.findPid(id)
-    console.log(pid)
-    if (pid[0].pid===0){
-      let data = await ctx.service.permission.secondAll(id)
-      for (let i=0;i<data.length;i++){
-        // 删除二级
-        await ctx.service.permission.delete(data[i].id)
-      }
-      // 删除二级的role_permission删除
-      
-      // 删除一级
-      await ctx.service.permission.delete(id)
-    }else{
-      await ctx.service.permission.delete(id)
-    }
-  }
+	async delete() {
+		const { ctx } = this
+		const { id } = ctx.query
+		const pid = await ctx.service.permission.findPid(id)
+		//需要删除的二级权限id
+		let deleteId = []
+		// 父类
+		if (pid[0].pid === 0) {
+			let secondId = await ctx.service.permission.secondAll(id)
+			for (let i = 0; i < secondId.length; i++) {
+				// 删除二级
+				let flag = await ctx.service.permission.delete(secondId[i].id)
+				deleteId.push(secondId[i].id)
+			}
+		} else {
+			deleteId.push(id)
+		}
+		// 删除二级的role_permission删除
+		let permissions = await ctx.service.rolePermission.findPermissionId()
+		let ids = []
+		for (let i = 0; i < permissions.length; i++) {
+			let permissionId = permissions[i].permission_id
+				.split(',')
+				.map(Number) 
+			let length = permissionId.length
+			for (let j = 0; j < deleteId.length; j++) {
+				permissionId.remove(deleteId[j])
+			}
+			if (length != permissionId.length) {
+				let h = getId()
+				const id = permissions[i].id
+				ids[h - 1] = { id, permissionId }
+			}
+		}
+		// 更新role_permission
+		for (const value of ids) {
+			let permissionIds = value.permissionId.toString()
+			await ctx.service.rolePermission.updatePermissionId(
+				value.id,
+				permissionIds
+			)
+		}
+		await ctx.service.permission.delete(id)
+	}
 }
+// 数组里面删除指定的元素的remove方法
+Array.prototype.indexOf = function (val) {
+	for (var i = 0; i < this.length; i++) {
+		if (this[i] == val) return i
+	}
+	return -1
+}
+Array.prototype.remove = function (val) {
+	var index = this.indexOf(val)
+	if (index > -1) {
+		this.splice(index, 1)
+	}
+}
+let getId = (function () {
+	let i = 0
+	return function () {
+		return ++i
+	}
+})()
 module.exports = PermissionController
--- app/service/role_permission.js
+++ app/service/role_permission.js
@@ -3,95 +3,39 @@
 const Service = require('egg').Service
 
 class RolePermissionService extends Service {
-  async all() {
-    const { ctx } = this
-    const rolepermission = await ctx.model.RolePermission.findAll()
-    return rolepermission
-  }
-
-  async one(id) {
-    const { ctx } = this
-    const rolepermission = await ctx.model.RolePermission.findByPk(id)
-    if (!rolepermission) {
-      ctx.throw(404, 'not found')
-    }
-    return rolepermission
-  }
-  async add(body) {
-    const { ctx } = this
-    const rolepermission = await ctx.model.RolePermission.create(body)
-    return rolepermission
-  }
-  async modify(id, body) {
-    const { ctx } = this
-    const rolepermission = await ctx.model.RolePermission.findByPk(id)
-    console.log(role)
-    return await rolepermission.update(body)
-  }
-  async delete(id) {
-    const rolepermission = await this.one(id)
-    return await rolepermission.destroy()
-  }
-
-  async createRoleIdPermissionId(body) {
-    const { ctx } = this
-    const rolePermission = await ctx.model.RolePermission.bulkCreate(body)
-    return rolePermission
-  }
-  /**
-   *根据role_id查询permission_id
-   *
-   * @param {*} id  role_id
-   * @return {*} 
-   * @memberof RolePermissionService
-   */
-  async selectPermissionId(id) {
-    const { ctx } = this
-    const permissionId = await ctx.model.RolePermission.findAll({
-      attributes: ['permission_id'],
-      where: {
-        role_id: id,
-      },
-    })
-    return permissionId
-  }
-  // 根据role_id查询出id
-  async selectId(id) {
-    const { ctx } = this
-    const permissionId = await ctx.model.RolePermission.findAll({
-      attributes: ['id'],
-      where: {
-        role_id: id,
-      },
-    })
-    return permissionId
-  }
-  async deleteRoleIdPermissionId(roleId,permissionId) {
-    const { ctx } = this
-    await ctx.model.RolePermission.destroy({
-      where: {
-        role_id:roleId,
-        permission_id: permissionId,
-      },
-    })
-  }
-  async deletePermissionId(roleId) {
-    const { ctx } = this
-    await ctx.model.RolePermission.destroy({
-      where: {
-        role_id:roleId,
-      },
-    })
-  }
-  async deleteRoleId(permissionId) {
-    const { ctx } = this
-    await ctx.model.RolePermission.destroy({
-      where: {
-        permission_id:permissionId,
-      },
-    })
-  }
-  
+	/**
+	 *  查询所有的role_permission表中的权限
+	 *
+	 * @memberof RolePermissionService
+	 */
+	async findPermissionId() {
+		const { ctx } = this
+		return await ctx.model.RolePermission.findAll({
+			attributes: ['id', 'permission_id'],
+			raw: true,
+		})
+	}
+	/**
+	 *根据id更新permissionId
+	 *
+	 * @param {*} id
+	 * @param {*} permissionIds
+	 * @return {*}
+	 * @memberof RolePermissionService
+	 */
+	async updatePermissionId(id, permissionIds) {
+		const { ctx } = this
+		return await ctx.model.RolePermission.update(
+			{
+				permission_id: permissionIds,
+			},
+			{
+				where: {
+					id: id,
+				},
+			}
+		)
+	}
 }
 
 module.exports = RolePermissionService
